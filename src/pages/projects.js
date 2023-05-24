f15e8f9bbd1b61b6d70b42181b2c55a1dad03ba7
Kene Maduka <32772014+kanayonickel@users.noreply.github.com>
2023-05-24 12:36:32 -0800

    Add files via upload

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,13 +5,15 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
-// import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
-import flixo from "../../public/images/projects/flix.png"
+
+
 import wibe from "../../public/images/projects/fashion-studio-website.jpg"
 import Nike from "../../public/images/projects/Nike.png"
+import xixori from "../../public/images/projects/flixori.png"
 import Delphia from "../../public/images/projects/anon.jpeg"
 import portfolio from "../../public/images/projects/portfolio-cover-image.jpg"
 import NFT from "../../public/images/projects/nft-collection-website-cover-image.jpg"
+import Bnb from "../../public/images/projects/airbnb.PNG"
 
 
 import {motion} from "framer-motion"
@@ -23,16 +25,17 @@ const FramerImage = motion(Image);
 
 const FeatureProject = ({type, title, summary, img, link, github}) => {
     return (
-        <article className='w-full flex items-center justify-between rounded-3xl border-solid border-dark bg-light shadow-2xl p-6'>
-          <Link href={link} target="_blank" className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
+        <article className='w-full flex items-center justify-between rounded-3xl border-solid border-dark bg-light shadow-2xl p-6 dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4 '>
+
+          <Link href={link} target="_blank" className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'>
           <FramerImage src={img} alt={title} className="w-full h-auto" whileHover={{scale:1.05}}  transition="{{duration:0.2}}" 
              priority sizes ="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw" />
           </Link>
 
-          <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-              <span className='text-primary font-medium text-xl'>{type}</span>
+          <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
+              <span className='text-primary font-medium text-xl dark:text-primaryDark xs:text-base'>{type}</span>
                   <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-                  <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
+                  <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm'>{title}</h2>
               </Link>
 
               <p className='my-2 font-medium text-dark'>{summary}</p>
@@ -40,7 +43,7 @@ const FeatureProject = ({type, title, summary, img, link, github}) => {
               <div className='mt-2 flex item-center'>
                   
                   <Link href={github} target="_blank" className="w-10"> <GithubIcon /> </Link>
-                  <Link href={link} target="_blank" className="ml-4 bg-dark rounded-lg text-light p-2 px-6 text-lg font-semibold"> Visit project </Link>
+                  <Link href={link} target="_blank" className="ml-4 bg-dark rounded-lg text-light p-2 px-6 text-lg font-semibold sm:px-4 sm:text-base"> Visit project </Link>
               </div>
           </div>
         </article>
@@ -58,7 +61,7 @@ const Project = ({title, type, img, link, github}) => {
           <div className='w-full flex flex-col items-start justify-between mt-4'>
               <span className='text-primary font-medium text-xl'>{type}</span>
                   <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-                  <h2 className='my-2 w-full text-left text-3xl font-bold'>{title}</h2>
+                  <h2 className='my-2 w-full text-left text-3xl font-bold sm:text-sm'>{title}</h2>
               </Link>
 
 
@@ -83,12 +86,13 @@ const projects = () => {
             
             <main className="w-full mb-16 flex flex-col items-center justify-cnter">
                 <Layout className="pt-16">
-                    <AnimatedText className="mb-10 lg" text="Imagination Trumps Success"/>
-                    <div className="grid grid-cols-1 2 gap-24">
+                    <AnimatedText className="mb-10 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" text="Imagination Trumps Knowledge"/>
+
+                    <div className="grid grid-cols-1 2 gap-24 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
                         <div className="col-span-12">
                            <FeatureProject   
                            title="Flixori Movie App"
-                           img={flixo}
+                           img={xixori}
                             summary="A simple fully responsive Movie web front-end using  Vanilla CSS and Javascript. This is an exercise on working a front end project without using Frameworks."
                             link="https://flixori.pages.dev/"
                             github="/"
@@ -101,8 +105,8 @@ const projects = () => {
                             title="Wibe Fashion Web App"
                             img={wibe}
                                 summary="Stunning Fashion Studio Website with React JS [ Locomotive Scroll + GSAP + Framer Motion ]. This projects showcases the power of going out of the box"
-                                link="wibe.pages.dev"
-                                github="/"
+                                link="https://wibe.pages.dev/"
+                                github="https://github.com/kanayonickel/fashionstudio"
                                 type="Project 1"/>
                          </div>
 
@@ -113,7 +117,7 @@ const projects = () => {
                             img={NFT}
                                 summary="A feature-rich NFT Collection Page."
                                 link="https://nftpsy.pages.dev/"
-                                github="/"
+                                github="https://github.com/kanayonickel/nftk"
                                 type="Project 2"/>
                          </div>    
 
@@ -124,8 +128,8 @@ const projects = () => {
                            title="Ecommerce Practice"
                            img={Delphia}
                             summary="Delphia is a fully responsive ecommerce website Landing page, maximum compatiblities in all mobile devices, built using HTML, CSS, and JavaScript.."
-                            link="/"
-                            github="/"
+                            link="https://delphia.pages.dev/"
+                            github="https://github.com/kanayonickel/Delphia"
                             type="Featured Project"/>
 
                          </div>   
@@ -134,8 +138,8 @@ const projects = () => {
                                 title="Portfolio Showcase"
                                 img={portfolio}
                                     summary="Delphia is a fully responsive ecommerce website Landing page, maximum compatiblities in all mobile devices, built using HTML, CSS, and JavaScript.."
-                                    link="http://localhost:3000/"
-                                    github="/"
+                                    link="/"
+                                    github="https://github.com/kanayonickel/newPort"
                                     type="Featured Project"/>
                          </div>
                          <div className="col-span-6">
@@ -144,9 +148,20 @@ const projects = () => {
                                 img={Nike}
                                     summary="A high-fidelity prototype of a Shoe page built with NextJS and framer motion.."
                                     link="https://nikemock.pages.dev "
-                                    github="/"
-                                    type="Featured Project"/>
+                                    github="https://github.com/kanayonickel/nike-landing"
+                                    type="Project"/>
                          </div>  
+
+                         <div className="col-span-12">
+                           <FeatureProject
+                           title="Mock AirBnB"
+                           img={Bnb}
+                            summary="A simple fully responsive Movie web front-end using  Tailwind CSS and Nextjs. "
+                            link="https://mockbnb.vercel.app//"
+                            github="https://github.com/kanayonickel/mockbnb"
+                            type="Featured Project"/>
+
+                         </div>
 
                     </div>
                 </Layout>
